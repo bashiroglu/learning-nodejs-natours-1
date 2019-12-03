eb7e2df9d4ba3665b829c9e51fd028661bd90275
Abdulla Bashiroglu <abdullabashir32@gmail.com>
2019-12-03 22:31:46 +0100

    added morgan which basically is package to manage middleware functions, and precisely morgan log the information about request to teh console

--- app.js
+++ app.js
@@ -1,22 +1,28 @@
 const fs = require('fs');
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 
+app.use(morgan('dev'));
 app.use(express.json());
+
+// app.use((res, req, next) => {
+//   console.log('hello from the middleware');
+//   next();
+// });
+
+// app.use((res, req, next) => {
+//   req.requestTime = new Date().toISOString();
+//   next();
+// });
 
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
-// app.get('/', (req, res) => {
-//   res.status(200).json({ message: 'hello from server', app: 'Natours' });
-// });
+const getAllTours = (req, res) => {
+  console.log(req.requestTime);
 
-// app.post('/', (req, res) => {
-//   res.json('post request');
-// });
-
-app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
     status: 'success',
     results: tours.length,
@@ -24,13 +30,10 @@ app.get('/api/v1/tours', (req, res) => {
       tours
     }
   });
-});
-app.get('/api/v1/tours/:id', (req, res) => {
-  // console.log(req.params);
+};
 
+const getTour = (req, res) => {
   const id = Number(req.params.id);
-  // if (id > tours.length) {
-
   const tour = tours.find(tour => tour.id === id);
   if (!tour) {
     return res.status(404).json({
@@ -44,8 +47,9 @@ app.get('/api/v1/tours/:id', (req, res) => {
       tour
     }
   });
-});
-app.patch('/api/v1/tours/:id', (req, res) => {
+};
+
+const updateTour = (req, res) => {
   const id = Number(req.params.id);
   if (id > tours.length) {
     return res.status(404).json({
@@ -59,8 +63,9 @@ app.patch('/api/v1/tours/:id', (req, res) => {
       tour: 'tour updated'
     }
   });
-});
-app.delete('/api/v1/tours/:id', (req, res) => {
+};
+
+const deleteTour = (req, res) => {
   const id = Number(req.params.id);
   if (id > tours.length) {
     return res.status(404).json({
@@ -72,13 +77,11 @@ app.delete('/api/v1/tours/:id', (req, res) => {
     status: 'success',
     data: null
   });
-});
+};
 
-app.post('/api/v1/tours', (req, res) => {
+const addNewTour = (req, res) => {
   const newId = tours[tours.length - 1].id + 1;
-  //   const newId = tours.length;
   const newTour = Object.assign({ id: newId }, req.body);
-
   tours.push(newTour);
   fs.writeFile(
     `${__dirname}/dev-data/data/tours-simple.json`,
@@ -92,7 +95,24 @@ app.post('/api/v1/tours', (req, res) => {
       });
     }
   );
-});
+};
+
+// app.get('/api/v1/tours', getAllTours);
+// app.get('/api/v1/tours/:id', getTour);
+// app.post('/api/v1/tours', addNewTour);
+// app.patch('/api/v1/tours/:id', updateTour);
+// app.delete('/api/v1/tours/:id', deleteTour);
+
+app
+  .route('/api/v1/tours')
+  .get(getAllTours)
+  .post(addNewTour);
+
+app
+  .route('/api/v1/tours/:id')
+  .get(getTour)
+  .patch(updateTour)
+  .delete(deleteTour);
 
 const port = 3000;
 app.listen(port, () => console.log(`server run in port: ${port}`));
