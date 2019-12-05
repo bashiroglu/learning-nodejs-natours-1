6173a1c69a7479a6ae6691ec87a18656fe5c0b3e
Abdulla Bashiroglu <abdullabashir32@gmail.com>
2019-12-05 14:58:16 +0100

    added database connection to config file, installed mongoose, used mongoose to creade Schema and model, added a tour to DB by using model

--- app.js
+++ app.js
@@ -1,8 +1,43 @@
+const mongoose = require('mongoose');
 const express = require('express');
 const morgan = require('morgan');
 
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+
+const DB = process.env.DATABASE_LOCAL;
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  })
+  .then(() => {
+    console.log('successful con.connections');
+  });
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'name is required'],
+    unique: true
+  },
+  rating: {
+    type: Number,
+    default: 4.5
+  },
+  price: {
+    type: Number,
+    required: [true, 'price is required']
+  }
+});
+const Tour = mongoose.model('Tour', tourSchema);
+const testTour = new Tour({ name: 'Xacmaz', rating: 4.7, price: 120 });
+testTour
+  .save()
+  .then(doc => {
+    console.log(doc);
+  })
+  .catch(err => console.log('error in saving document', err));
 
 const app = express();
 
