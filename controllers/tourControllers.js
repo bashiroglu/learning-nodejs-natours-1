02fd71d320a4c8afcc73acf506af1fbc7d9efad8
Abdulla Bashiroglu <abdullabashir32@gmail.com>
2019-12-06 18:18:42 +0100

    added 2 fields(selecting) function which allow user to select properties by their names

--- controllers/tourControllers.js
+++ controllers/tourControllers.js
@@ -21,6 +21,13 @@ exports.getAllTours = async (req, res) => {
     } else {
       query = query.sort('createdAt');
     }
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v');
+    }
+
     const tours = await query;
 
     //send response
