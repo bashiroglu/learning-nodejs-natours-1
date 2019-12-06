ae4d7647472c03bc93a431eeb14c4fb02a3f4386
Abdulla Bashiroglu <abdullabashir32@gmail.com>
2019-12-06 23:01:22 +0100

    added pagination function which handle which page user request and limit of data

--- controllers/tourControllers.js
+++ controllers/tourControllers.js
@@ -27,7 +27,16 @@ exports.getAllTours = async (req, res) => {
     } else {
       query = query.select('-__v');
     }
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 100;
+    const skip = (page - 1) * limit;
 
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numsOfTour = await Tour.countDocuments();
+      if (skip >= numsOfTour) throw new Error('this is all we have');
+    }
     const tours = await query;
 
     //send response
