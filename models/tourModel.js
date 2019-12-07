a106fc6bffe5f1c334e69aefd6db66bad9d5dc17
Abdulla Bashiroglu <abdullabashir32@gmail.com>
2019-12-07 12:56:13 +0100

    added virtual function to get different but also independent data from db

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,53 +1,63 @@
 const mongoose = require('mongoose');
 
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'name is required'],
-    unique: true,
-    trim: true
+const tourSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'name is required'],
+      unique: true,
+      trim: true
+    },
+    maxGroupSize: {
+      type: Number,
+      required: [true, 'maxGroupSize is required']
+    },
+    difficulty: {
+      type: String,
+      required: [true, 'difficulty is required']
+    },
+    duration: {
+      type: Number,
+      required: [true, 'duration is required']
+    },
+    ratingsAverage: {
+      type: Number,
+      default: 4.5
+    },
+    ratingsQuantity: {
+      type: Number,
+      default: 0
+    },
+    price: {
+      type: Number,
+      required: [true, 'price is required']
+    },
+    priceDiscount: Number,
+    summary: {
+      type: String,
+      trim: true,
+      required: [true, 'summary is required']
+    },
+    imageCover: {
+      type: String,
+      required: [true, 'summary is required']
+    },
+    images: [String],
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      select: false
+    },
+    startDates: [Date]
   },
-  maxGroupSize: {
-    type: Number,
-    required: [true, 'maxGroupSize is required']
-  },
-  difficulty: {
-    type: String,
-    required: [true, 'difficulty is required']
-  },
-  duration: {
-    type: Number,
-    required: [true, 'duration is required']
-  },
-  ratingsAverage: {
-    type: Number,
-    default: 4.5
-  },
-  ratingsQuantity: {
-    type: Number,
-    default: 0
-  },
-  price: {
-    type: Number,
-    required: [true, 'price is required']
-  },
-  priceDiscount: Number,
-  summary: {
-    type: String,
-    trim: true,
-    required: [true, 'summary is required']
-  },
-  imageCover: {
-    type: String,
-    required: [true, 'summary is required']
-  },
-  images: [String],
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-    select: false
-  },
-  startDates: [Date]
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true }
+  }
+);
+
+tourSchema.virtual('durationWeeks').get(function() {
+  return this.duration / 7;
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
