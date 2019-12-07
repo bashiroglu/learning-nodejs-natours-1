98199062716d2779b5577bc8af422197cd37a693
Abdulla Bashiroglu <abdullabashir32@gmail.com>
2019-12-07 13:14:24 +0100

    downloaded slugify and added slugify middleware function to the Tourmodel

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -8,6 +9,7 @@ const tourSchema = new mongoose.Schema(
       unique: true,
       trim: true
     },
+    slug: String,
     maxGroupSize: {
       type: Number,
       required: [true, 'maxGroupSize is required']
@@ -58,6 +60,10 @@ const tourSchema = new mongoose.Schema(
 
 tourSchema.virtual('durationWeeks').get(function() {
   return this.duration / 7;
+});
+tourSchema.pre('save', function(next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
