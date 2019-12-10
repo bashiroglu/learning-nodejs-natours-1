677d5cbaf304527412b1322840200fade83eabea
Abdulla Bashiroglu <abdullabashir32@gmail.com>
2019-12-10 01:00:58 +0100

    added error handler function
    error handler middleware function use take all errors and send response

--- app.js
+++ app.js
@@ -15,9 +15,22 @@ app.use(express.static(`${__dirname}/public`));
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.all('*', (req, res, next) => {
-  res.status(404).json({
-    status: 'failed',
-    message: `can not find ${req.originalUrl}`
+  // res.status(404).json({
+  //   status: 'failed',
+  //   message: `can not find ${req.originalUrl}`
+  // });
+  const err = new Error(`can not find ${req.originalUrl}`);
+  err.status = 'fail';
+  err.statusCode = 404;
+  next(err);
+});
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message
   });
 });
 
