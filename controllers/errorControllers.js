c74d8a8f8d68843fac8969a06df9f5ddc3db73c7
Abdulla Bashiroglu <abdullabashir32@gmail.com>
2019-12-11 15:50:54 +0100

    updated error controller to handle post tour request  and error with validtions
    We identified this error by checking its name property and responsed based on that condition by using our function which use object.values method to extract all messages and Apperror class

--- controllers/errorControllers.js
+++ controllers/errorControllers.js
@@ -13,6 +13,13 @@ const handleDublicateErrorDB = err => {
   return new AppError(message, 400);
 };
 
+const handleValidationErrorDB = err => {
+  const errors = Object.values(err.errors).map(el => el.message);
+  const message = `Invalid input ${errors.join('. ')}`;
+
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -50,6 +57,8 @@ module.exports = (err, req, res, next) => {
     let error = { ...err };
     if (error.name === 'CastError') error = handleCastErrorDB(error);
     if (error.code === 11000) error = handleDublicateErrorDB(error);
+    if (error.name === 'ValidationError')
+      error = handleValidationErrorDB(error);
 
     sendErrorProd(error, res);
   }
