7d14226156259201b1d569b9107d8e0e4e6e23ae
Abdulla Bashiroglu <abdullabashir32@gmail.com>
2019-12-11 13:29:02 +0100

    imporved error controllers file to handle different errors differently
    we catogarized errors in prod and dev, because we don't send much data while propduction logs which we send from else block can be seen in our server console, operational errors we can send but not programming becasuse it may be dangerous

--- controllers/errorControllers.js
+++ controllers/errorControllers.js
@@ -1,9 +1,38 @@
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack
+  });
+};
+const sendErrorProd = (err, res) => {
+  //operational error that we can send
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message
+    });
+  } else {
+    //we don't send this kind of errors
+    //for us
+    console.error('Error from Error controller');
+
+    //send response
+    res.status(500).json({
+      status: 'error',
+      message: 'something went wrong'
+    });
+  }
+};
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
-
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message
-  });
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrorProd(err, res);
+  }
 };
+
