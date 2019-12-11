35dd8cece4788b8524be21c35f9cf3235ce3e2b1
Abdulla Bashiroglu <abdullabashir32@gmail.com>
2019-12-11 20:34:28 +0100

    added auth user handler and added signup router to the user routes file

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const userControllers = require('../controllers/userControllers');
+const authControllers = require('../controllers/authControllers');
+
 const router = express.Router();
+
+router.post('/signup', authControllers.signup);
+
 router
   .route('/')
   .get(userControllers.getAllUsers)
