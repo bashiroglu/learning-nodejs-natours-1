8e43883601f575e7b2ce71708eab6ce240663b46
Abdulla Bashiroglu <abdullabashir32@gmail.com>
2019-12-19 00:25:23 +0100

    added reviewModel and notes about revieModel

---
+++ models/reviewModel.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+
+const reviewSchema = new mongoose.Schema(
+  {
+    review: {
+      type: String,
+      required: [true, 'review is required']
+    },
+
+    rating: {
+      type: Number,
+      min: 1,
+      max: 5
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now()
+    },
+    tour: [
+      /* reference to tour */
+      {
+        type: mongoose.Schema.ObjectId,
+        ref: 'Tour',
+        required: [true, 'review have to have tour']
+      }
+    ],
+    user: [
+      /* reference to user */
+      {
+        type: mongoose.Schema.ObjectId,
+        ref: 'User',
+        required: [true, 'review have to have user']
+      }
+    ]
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true }
+  }
+);
+const Review = mongoose.model('Review', reviewSchema);
+module.exports = Review;
