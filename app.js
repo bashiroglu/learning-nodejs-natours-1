9d1b8de6f998dc4a23c4d2c9076dc185e46bad58
Abdulla Bashiroglu <abdullabashir32@gmail.com>
2019-12-21 16:31:32 +0100

    notes about pug file and simple pug codes

--- app.js
+++ app.js
@@ -51,7 +51,10 @@ app.use(
 app.use(express.json({ limit: '10kb' }));
 
 app.get('/', (req, res) => {
-  res.status(200).render('base');
+  res.status(200).render('base', {
+    tour: 'Quba',
+    user: 'Abdulla'
+  });
 });
 
 app.use('/api/v1/tours', tourRouter);
