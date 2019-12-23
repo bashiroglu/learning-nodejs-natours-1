df69a52ef2bf889e9eb7f097d7fb556c98c4d717
Abdulla Bashiroglu <abdullabashir32@gmail.com>
2019-12-23 19:16:56 +0100

    added production error handler with message

--- controllers/errorControllers.js
+++ controllers/errorControllers.js
@@ -27,40 +27,62 @@ const handleValidationErrorDB = err => {
 
   return new AppError(message, 400);
 };
-
 const sendErrorDev = (err, req, res) => {
+  // A) API
   if (req.originalUrl.startsWith('/api')) {
-    res.status(err.statusCode).json({
+    return res.status(err.statusCode).json({
       status: err.status,
       error: err,
       message: err.message,
       stack: err.stack
     });
-  } else {
-    res.status(err.statusCode).render('error', {
+  }
+
+  // B) RENDERED WEBSITE
+  console.error('ERROR 💥', err);
+  return res.status(err.statusCode).render('error', {
+    title: 'Something went wrong!',
+    msg: err.message
+  });
+};
+
+const sendErrorProd = (err, req, res) => {
+  // A) API
+  if (req.originalUrl.startsWith('/api')) {
+    // A) Operational, trusted error: send message to client
+    if (err.isOperational) {
+      return res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message
+      });
+    }
+    // B) Programming or other unknown error: don't leak error details
+    // 1) Log error
+    console.error('ERROR 💥', err);
+    // 2) Send generic message
+    return res.status(500).json({
+      status: 'error',
+      message: 'Something went very wrong!'
+    });
+  }
+
+  // B) RENDERED WEBSITE
+  // A) Operational, trusted error: send message to client
+  if (err.isOperational) {
+    console.log(err);
+    return res.status(err.statusCode).render('error', {
       title: 'Something went wrong!',
       msg: err.message
     });
   }
-};
-const sendErrorProd = (err, res) => {
-  //operational error that we can send
-  if (err.isOperational) {
-    res.status(err.statusCode).json({
-      status: err.status,
-      message: err.message
-    });
-  } else {
-    //we don't send this kind of errors
-    //for us
-    console.error('Error from Error controller');
-
-    //send response
-    res.status(500).json({
-      status: 'error',
-      message: 'something went wrong(prod)'
-    });
-  }
+  // B) Programming or other unknown error: don't leak error details
+  // 1) Log error
+  console.error('ERROR 💥', err);
+  // 2) Send generic message
+  return res.status(err.statusCode).render('error', {
+    title: 'Something went wrong!',
+    msg: 'Please try again later.'
+  });
 };
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
@@ -68,7 +90,10 @@ module.exports = (err, req, res, next) => {
   if (process.env.NODE_ENV === 'development') {
     sendErrorDev(err, req, res);
   } else if (process.env.NODE_ENV === 'production') {
-    let error = { ...err };
+    let error = {
+      ...err
+    }; /* for some reason cannot copy the message, jonas didn't explain why */
+    error.message = err.message;
     if (error.name === 'CastError') error = handleCastErrorDB(error);
     if (error.code === 11000) error = handleDublicateErrorDB(error);
     if (error.name === 'ValidationError')
