4db337d7faf620ee5282ca9ae9f3ab64a402e51d
Abdulla Bashiroglu <abdullabashir32@gmail.com>
2019-12-25 15:19:35 +0100

    modified server.js

--- server.js
+++ server.js
@@ -11,7 +11,11 @@ dotenv.config({ path: './config.env' });
 
 const app = require('./app');
 
-const DB = process.env.DATABASE_LOCAL;
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
 mongoose
   .connect(DB, {
     useNewUrlParser: true,
